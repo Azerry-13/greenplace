b68906b66436b6c76592660f6618cb528e1451d0
Dmitrii <13azerry@gmail.com>
2024-03-01 15:11:15 +0600

    Правки js

--- scripts/about.js
+++ scripts/about.js
@@ -1,13 +1,13 @@
 function changeImageByScreenSize() {
     let imageElement = document.getElementById('responsive-image-about-1');
     let imageElement2 = document.getElementById('responsive-image-about-2');
-    if (window.innerWidth < 361) {
+    if (window.innerWidth < 550) {
       imageElement.src = 'src/about-image-1-mobile.png';
     } else {
       imageElement.src = 'src/about-image-1.png';
     }
 
-    if (window.innerWidth < 361) {
+    if (window.innerWidth < 550) {
         imageElement2.src = 'src/about-image-2-mobile.png';
       } else {
         imageElement2.src = 'src/about-image-2.png';
